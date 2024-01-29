9532d07caac636ccc37a7a6ef2b8f20d29627c33
EgorT <you@example.com>
2024-01-29 23:50:11 +0300

    fixes

--- brain_games/games/brain_prime_logic.py
+++ brain_games/games/brain_prime_logic.py
@@ -4,7 +4,7 @@ from random import randint
 def brain_prime():
 
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    number = randint(1, 100)
+    number = randint(2, 100)
     right_answer = 'yes'
     question = f'Question: {number}\nYour answer: '
 
